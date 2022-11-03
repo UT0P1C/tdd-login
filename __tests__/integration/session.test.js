391cf4f8f834fd6a65e60caa3b7e542848d118bd
UT0P1C <ljonham@gmail.com>
2022-11-02 21:40:33 -0300

    first commit

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,30 @@
+const request = require("supertest");
+
+const app = require("../../src/app")
+
+const {User} = require('../../src/app/models');
+
+const truncate = require("../utils/truncate");
+
+describe("Authentication suit", () => {
+	beforeEach(async () => {
+		await truncate();
+	});
+	
+	it("should authenticate with valid credentials", async() => {
+		const user = User.create({
+			name: "Esq1z0",
+			email: "esq1z0@esq1z0.com",
+			password_hash: "12312313"
+		});
+
+		const response = await request(app)
+								.post("/sessions")
+								.send({
+									email: user.email,
+									password: '123456'
+								});
+		
+		expect(response.status).toBe(200);
+	});
+});
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+const routes = require("express").Router();
+
+routes.post("/sessions", (req, res) => {
+	
+});
+
+module.exports = routes;
